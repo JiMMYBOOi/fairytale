0983d8993c43699dc34cd187d743060927e1d718
Christian Schneider <schnaader@gmx.de>
2018-03-19 12:42:56 +0100

    AppVeyor...

--- make.bat
+++ make.bat
@@ -6,8 +6,7 @@ REM "make 32" for a 32-bit compile
 
 REM g++ 32-bit/64-bit commands - change them according to your environment
 set GPP32=g++
-set GPP64=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\bin\g++
-
+set GPP64=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\bin\g++
 
 set GCC=%GCC64%
 set GPP=%GPP64%
